d35271fc20891e3757c4e7082ad255dbaad1d45d
SouJhin <117504437+SouJhin@users.noreply.github.com>
2023-02-09 23:43:10 +0800

    Initial commit
    GO

---
+++ main.go
@@ -0,0 +1,21 @@
+/*
+ * @Author: SouJhin soujhin2022@gmail.com
+ * @Date: 2023-02-09 23:44:00
+ * @LastEditors: SouJhin soujhin2022@gmail.com
+ * @LastEditTime: 2023-02-10 09:08:22
+ * @FilePath: \GOPRO\2.9\main.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package main
+
+import (
+	"fmt"
+
+	"mypro/user"
+)
+
+func main() {
+	fmt.Println("fuck u")
+	s := user.Hello()
+	fmt.Printf("s: %v\n", s)
+}
---
+++ user/user.go
@@ -0,0 +1,13 @@
+/*
+ * @Author: SouJhin soujhin2022@gmail.com
+ * @Date: 2023-02-09 23:55:21
+ * @LastEditors: SouJhin soujhin2022@gmail.com
+ * @LastEditTime: 2023-02-09 23:55:39
+ * @FilePath: \GO_PRO\2.9\user.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package user
+
+func Hello() string {
+	return "fuck UUUUU"
+}
